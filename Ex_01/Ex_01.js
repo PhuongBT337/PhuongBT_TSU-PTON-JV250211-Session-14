c918c3ada8913bd5b96f7a71406b8c8541a01295
PhuongBT337 <b.t.phuong337@gmail.com>
2025-04-01 03:24:47 +0900

    update bai tap 01

---
+++ Ex_01/Ex_01.js
@@ -0,0 +1,9 @@
+let person = {
+    name: "John Doe",
+    age: 25,
+    job: "Developer",
+}
+
+console.log(`Ten toi la ${person.name}`);
+console.log(`Toi ${person.age}`);
+console.log(`Toi dang la mot ${person.job}`);
---
+++ Ex_02/Ex_02.js
@@ -0,0 +1,21 @@
+// Tạo một đối tượng car với các thuộc tính sau:
+
+// brand: "Toyota"
+// model: "Corolla"
+// year: 2020
+// Thêm một thuộc tính mới là color với giá trị "red"
+// Cập nhật giá trị của thuộc tính year thành 2022
+// In ra đối tượng sau khi cập nhật
+
+let car = {
+    brand: `Toyota`,
+    model: `Corolla`,
+    year: 2020
+}
+
+car.color=`red`;
+console.log(car);
+
+car.year=`2022`;
+console.log(car);
+
